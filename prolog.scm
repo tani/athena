e47bd01d490a859c4f714dd56acafee4d790c1ff
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-06-18 14:43:04 +0900

    refactor solve-*

--- prolog.scm
+++ prolog.scm
@@ -333,28 +333,32 @@
         (current-dynamic-parameters new-dynamic-parameters)
         new-parameter))))
 
+
+(define (prepare-goals-with-cut goals cut-point)
+  (let* ((renamed-goals (replace-anonymous-variables goals))
+         (cut-goals (insert-cut-point renamed-goals cut-point)))
+    (prove-all cut-goals '())))
+
+(define (make-initial-continuation goals)
+  (lambda ()
+    (let ((cont (lambda (cut-point) (prepare-goals-with-cut goals cut-point))))
+      (call/cc cont))))
+
 (define (solve-first goals term)
-  (let ((result
-          (call/cc
-            (lambda (cut-point)
-              (let ((new-goals (insert-cut-point (replace-anonymous-variables goals) cut-point)))
-                (prove-all new-goals '()))))))
+  (let* ((initial-cont (make-initial-continuation goals))
+         (result (initial-cont)))
     (and (not (failure? result))
          (substitute-bindings (success-bindings result) term))))
 
 (define (solve-all goals term)
-  (let loop ((continuation
-               (lambda ()
-                 (call/cc
-                   (lambda (cut-point)
-                     (let ((new-goals (insert-cut-point (replace-anonymous-variables goals) cut-point)))
-                       (prove-all new-goals '()))))))
-             (accumulator '()))
-    (let ((result (continuation)))
-      (if (failure? result)
-          (reverse accumulator)
-          (loop (success-continuation result)
-                (cons (substitute-bindings (success-bindings result) term) accumulator))))))
+  (let ((initial-cont (make-initial-continuation goals)))
+    (let loop ((continuation initial-cont) (accumulator '()))
+      (let ((result (continuation)))
+        (if (failure? result)
+            (reverse accumulator)
+            (let* ((value (substitute-bindings (success-bindings result) term))
+                   (next (success-continuation result)))
+              (loop next (cons value accumulator))))))))
 
 (define current-solution-accumulator (make-parameter '()))
 (define current-lisp-environment (make-parameter #f))
