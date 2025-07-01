69d744ee4ac3d7332cacf8b45cf00adbda6814d8
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-07-01 00:52:02 +0000

    remove patch

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 pushd gambit
 make clean
-cp ../use_npm.patch .
-git apply use_npm.patch
 ./configure --enable-single-host --enable-targets=js --enable-defailt-compile-options="(compactness 9)" --prefix=`pwd`
 make -j8
 make doc TEXI2HTML=`which texi2html`
