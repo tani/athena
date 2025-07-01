87312198256cc7bf8eeb20cdd73ffba9ea121268
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-07-01 07:08:30 +0000

    Add prolog to browserfs

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
+git -C gambit apply ../index_html.patch
+make -C gambit clean
 pushd gambit
-make clean
-./configure --enable-single-host --enable-targets=js --enable-defailt-compile-options="(compactness 9)" --prefix=`pwd`
-make -j8
-make doc TEXI2HTML=`which texi2html`
-make modules
-make -C contrib/try try
+./configure \
+  --enable-single-host \
+  --enable-targets=js \
+  --enable-defailt-compile-options="(compactness 9)" \
+  --prefix=`pwd`
 popd
+make -C gambit -j8
+make -C gambit doc TEXI2HTML=`which texi2html`
+make -C gambit modules
+make -C gambit/contrib/try try
+cp src/prolog.* gambit/contrib/try/try
