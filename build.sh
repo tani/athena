61f2d2fadcbb985ab4e7e3311cb34d2e4874c568
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-07-01 09:07:59 +0000

    Use nproc

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ pushd gambit
   --enable-defailt-compile-options="(compactness 9)" \
   --prefix=`pwd`
 popd
-make -C gambit -j8
+make -C gambit -j`nproc`
 make -C gambit doc TEXI2HTML=`which texi2html`
 make -C gambit modules
 make -C gambit/contrib/try try
