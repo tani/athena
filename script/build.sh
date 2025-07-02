f9675ef632d41795c75ec33bf9184cebef98c97a
TANIGUCHI Masaya <ta2gch@gmail.com>
2025-07-02 05:34:07 +0000

    build.sh -> script/build.sh

---
+++ script/build.sh
@@ -0,0 +1,14 @@
+pushd gambit
+git apply ../index_html.patch
+make clean
+./configure \
+  --enable-single-host \
+  --enable-targets=js \
+  --enable-defailt-compile-options="(compactness 9)" \
+  --prefix=`pwd`
+make -j`nproc`
+make doc TEXI2HTML=`which texi2html`
+make modules
+make -C contrib/try try
+cp src/prolog.* gambit/contrib/try/try
+popd
